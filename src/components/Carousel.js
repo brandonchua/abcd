a363bc5c8fb2a92d1d06dc2f49845700d93be2f2
Brandon Chua <mrbrandonchua@gmail.com>
2022-10-23 13:08:37 +0800

    Added draft bears

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,7 +9,7 @@ import "swiper/css/navigation";
 
 
 import { Pagination, Navigation, Autoplay, EffectCards } from "swiper";
-
+/*
 import img1 from '../assets/Nfts/bighead.svg';
 import img2 from '../assets/Nfts/bighead-1.svg';
 import img3 from '../assets/Nfts/bighead-2.svg';
@@ -20,6 +20,17 @@ import img7 from '../assets/Nfts/bighead-6.svg';
 import img8 from '../assets/Nfts/bighead-7.svg';
 import img9 from '../assets/Nfts/bighead-8.svg';
 import img10 from '../assets/Nfts/bighead-9.svg';
+*/
+import img1 from "../../assets/Nfts/1.png";
+import img2 from "../../assets/Nfts/2.png";
+import img3 from "../../assets/Nfts/3.png";
+import img4 from "../../assets/Nfts/4.png";
+import img5 from "../../assets/Nfts/5.png";
+import img6 from "../../assets/Nfts/6.png";
+import img6 from "../../assets/Nfts/7.png";
+import img6 from "../../assets/Nfts/8.png";
+import img6 from "../../assets/Nfts/9.png";
+import img6 from "../../assets/Nfts/10.png";
 
 import Arrow from '../assets/Arrow.svg';
 
--- src/components/sections/Banner.js
+++ src/components/sections/Banner.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import img1 from "../../assets/Nfts/bighead.svg";
+/*import img1 from "../../assets/Nfts/bighead.svg";
 import img2 from "../../assets/Nfts/bighead-1.svg";
 import img3 from "../../assets/Nfts/bighead-2.svg";
 import img4 from "../../assets/Nfts/bighead-3.svg";
 import img5 from "../../assets/Nfts/bighead-4.svg";
 import img6 from "../../assets/Nfts/bighead-5.svg";
+*/
+import img1 from "../../assets/Nfts/1.png";
+import img2 from "../../assets/Nfts/2.png";
+import img3 from "../../assets/Nfts/3.png";
+import img4 from "../../assets/Nfts/4.png";
+import img5 from "../../assets/Nfts/5.png";
+import img6 from "../../assets/Nfts/6.png";
 
 const Section = styled.section`
 width: 100vw;
